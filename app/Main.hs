a38d6a997ef90f841d7f7d8534708043715c17fe
daemon <0xDAEF0F@proton.me>
2023-05-27 13:34:42 -0600

    27_05_2023

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,17 @@
 module Main where
 
 import qualified CalorieCounting (sumTop3)
+import qualified CampCleanupA (solution)
+import qualified CampCleanupB (solution)
 import qualified RockPaperScissors (fight)
 import qualified RockPaperScissorsTwo (fight)
 import qualified RucksackReorganizationA (getSumOfPriorities)
 import qualified RucksackReorganizationB (solution)
-import qualified CampCleanupA (solution)
-import qualified CampCleanupB (solution)
+import qualified SupplyStacksA (solution)
 import System.IO ()
 
 main :: IO ()
 main = do
-  content <- readFile "input-day-4.txt"
-  let sum = CampCleanupB.solution content
-  print sum
+  content <- readFile "input-day-5.txt"
+  let solution = SupplyStacksA.solution content
+  print solution
---
+++ src/SupplyStacksA.hs
@@ -0,0 +1,57 @@
+module SupplyStacksA (solution) where
+
+type Quantity = Int
+
+type From = Int
+
+type To = Int
+
+type Instruction = (Quantity, From, To)
+
+type CratesState = [[Char]]
+
+--                     [L]     [H] [W]
+--                 [J] [Z] [J] [Q] [Q]
+-- [S]             [M] [C] [T] [F] [B]
+-- [P]     [H]     [B] [D] [G] [B] [P]
+-- [W]     [L] [D] [D] [J] [W] [T] [C]
+-- [N] [T] [R] [T] [T] [T] [M] [M] [G]
+-- [J] [S] [Q] [S] [Z] [W] [P] [G] [D]
+-- [Z] [G] [V] [V] [Q] [M] [L] [N] [R]
+--  1   2   3   4   5   6   7   8   9
+
+initialState :: CratesState
+initialState =
+  [ ['S', 'P', 'W', 'N', 'J', 'Z'],
+    ['T', 'S', 'G'],
+    ['H', 'L', 'R', 'Q', 'V'],
+    ['D', 'T', 'S', 'V'],
+    ['J', 'M', 'B', 'D', 'T', 'Z', 'Q'],
+    ['L', 'Z', 'C', 'D', 'J', 'T', 'W', 'M'],
+    ['J', 'T', 'G', 'W', 'M', 'P', 'L'],
+    ['H', 'Q', 'F', 'B', 'T', 'M', 'G', 'N'],
+    ['W', 'Q', 'B', 'P', 'C', 'G', 'D', 'R']
+  ]
+
+solution :: String -> CratesState
+solution st = foldl executeInstruction initialState allInstructions
+  where
+    allInstructions = map parseInstruction $ lines st
+
+parseInstruction :: String -> Instruction
+parseInstruction s = (read qty, read from - 1, read to - 1)
+  where
+    [_, qty, _, from, _, to] = words s
+
+executeInstruction :: CratesState -> Instruction -> CratesState
+executeInstruction cr (0, _, _) = cr
+executeInstruction cr (qty, from, to) = executeInstruction newStacks (qty - 1, from, to)
+  where
+    crateToMove = head (cr !! from)
+    newFromStack = tail (cr !! from)
+    newToStack = crateToMove : (cr !! to)
+    newStacks = map foo $ zip cr [0 ..]
+    foo (st, i)
+      | i == from = newFromStack
+      | i == to = newToStack
+      | otherwise = st
